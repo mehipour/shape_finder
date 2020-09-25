6aab49799507ca13a1e821ff345e1696d0f01321
mehipour <mehrdad.pourfathi@gmail.com>
2020-09-25 12:03:31 -0400

    added corner handling function.

--- shape_finder.py
+++ shape_finder.py
@@ -2,9 +2,9 @@
 # default parameters:
 #   python shape_finder.py
 # custom parameters:
-#   python shape_finder.py --circles 2 --squares 2 --triangles 2 --seed 0
+#   python shape_finder.py --circles 2 --squares 2 --triangles 1 --seed 0
 # simplified parameters values
-#   python shape_finder.py -c 2 -sq 2 -t 2 -sd 0
+#   python shape_finder.py -c 2 -sq 2 -t 1 -sd 823
 #
 # Create by Mehrdad Pourfathi
 # Date: 9/23/2020
@@ -13,13 +13,12 @@
 import numpy as np
 import cv2
 # import skimage.segmentation as seg
-import logging
 import argparse
-import matplotlib.pyplot as plt
 import active_contour
+import itertools
+from utils import *
+import time
 
-# setup log configuration.
-logging.basicConfig(level=logging.DEBUG, filemode='w', filename='ouptut.log')
 
 # construct argument parser
 ap = argparse.ArgumentParser()
@@ -34,36 +33,6 @@ ap.add_argument('-sd', '--seed', type=int, default=1,
 args = vars(ap.parse_args())
 
 
-def create_template(img, img_color):
-    template_image = np.ones_like(img, dtype='uint8')*255
-    template_image = cv2.rectangle(template_image, (240, 320),(240, 320), 0, 1)
-    contours, hierarchy = cv2.findContours(template_image, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
-    print(hierarchy)
-    # remove the alrgest contour since it is the picture's frame
-    contours = sorted(contours, key=cv2.contourArea, reverse=False)[:-1]
-
-    for c in contours:
-        template_contour = cv2.convexHull(c)
-        # cv2.drawContours(img_color,[template_contour],0,(0,255,0),2)
-    # cv2.matchShapes(contour template, contour method, method parameter)
-
-    # find contours in the image
-    contours, _ = cv2.findContours(img, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
-    contours = sorted(contours, key=cv2.contourArea, reverse=False)[:-1]
-
-    for c in contours:
-        match = cv2.matchShapes(template_contour, c, 1, 0.0)
-        print(match)
-        if 0.01 < match < 5:
-            cv2.drawContours(img_color,[c],0,(0,255,0),2)
-            cv2.imshow('matched shapes', img_color)
-
-         # continue video feed until 'q' is pressed
-        key = cv2.waitKey(1) & 0xFF
-        # if 'n' is pressed count side go to new side
-        if key == ord('n'):
-            continue
-
 def image_maker(circles, squares, triangles, seed, res=(480,640)):
     '''
     creates a black and white image made with randomly placed circles,
@@ -76,22 +45,22 @@ def image_maker(circles, squares, triangles, seed, res=(480,640)):
     # put random circles
     for i in range(int(circles)):
         # select the position and radius of the circles with uniform distribution.
-        x = np.random.randint(100,540)
-        y = np.random.randint(100,380)
-        r = np.random.randint(20,50)
-        cv2.circle(img_color, (x,y), r, 0, -1)
+        x = np.random.randint(100,380)
+        y = np.random.randint(100,430)
+        r = np.random.randint(40,80)
+        cv2.circle(img_color, (x,y), r, 0, 1)
     # put random rectangles
     for i in range(int(squares)):
         # randomly select the edges of the rectangle
-        x = np.random.randint(100,540,1)
-        y = np.random.randint(100,380,1)
-        d = np.random.randint(20,50,1)
-        cv2.rectangle(img_color, (x, x+d), (y, y+d), 0, -1)
+        x = np.random.randint(100,380,1)
+        y = np.random.randint(100,430,1)
+        d = np.random.randint(40,80,1)
+        cv2.rectangle(img_color, (x, x+d), (y, y+d), 0, 1)
     # put random triangles
     for i in range(int(triangles)):
         # randomly select 3 vertices to generate a triangle.
         vertices = np.random.randint(40, 480, (3,2))
-        cv2.drawContours(img_color, [vertices], 0, 0, -1)
+        cv2.drawContours(img_color, [vertices], 0, 0, 1)
 
     # return the grau scale image
     img = cv2.cvtColor(img_color, cv2.COLOR_BGR2GRAY)
@@ -99,14 +68,52 @@ def image_maker(circles, squares, triangles, seed, res=(480,640)):
     return img, img_color
 
 
-def laplacian_filter(img):
+def find_important_points(image, param=0.4, max_points = 30):
     '''
-    Applies a 2D laplacian filter on the image
+    find corner and cross points
     '''
-    kernel = np.array([[1, 1, 1], [1, -8, 1], [1, 1, 1]], dtype=np.float32)
-    img = cv2.filter2D(img, -1, kernel)
+    corners = cv2.goodFeaturesToTrack(img, max_points, param, 5)
+    corners = [list(i.ravel()) for i in corners] 
+    return image, corners
 
-    return img
+def draw_corners(image, corners, label='corners'):
+    '''
+    draws corners.
+    '''
+    for corner in corners:
+        x,y = corner[0], corner[1]
+        cv2.circle(image, (x,y),5,0,-1)
+    cv2.imshow(label, image)
+    # return image
+
+
+def find_squares(img, image_color, corners):
+    '''
+    find squares. 
+    '''
+    new_corners = []
+    # select four points 
+    for pts in itertools.permutations(corners, 4):
+        list_of_four_corners = [[list(pp.ravel()) for pp in pts]]
+        # print(pts)
+        p = np.array(list_of_four_corners, np.int32)
+        img_square = img.copy()
+        # create a polygon with 4 points
+        img_square = cv2.polylines(img_square, [p], True, (0,255,0), thickness=3)
+        perimeter = cv2.arcLength(p, True)
+        approx = cv2.approxPolyDP(p, 0.1 * perimeter, True)
+        # create the convex hull of the polygon
+        squares = cv2.convexHull(approx)
+        # check if square
+        if len(approx) == 4 and check_if_square(approx, 0.1, [87, 93]):
+            cv2.drawContours(img_color, [squares], 0, (0,255,0), 1)
+        else:
+            # add points to new corner list
+            for i in list_of_four_corners:
+                if i not in new_corners:
+                    new_corners.append(i)
+    cv2.imshow('squares', img_color)
+    return new_corners
 
 
 def circle_finder(img, img_color):
@@ -135,70 +142,25 @@ def circle_finder(img, img_color):
     return circles, output
 
 
-def segment_shapes(img):
-    '''
-    segment circles, rectangles and triangles and draw their boundaries.
-    '''
-
-
-    # find corders and points crossing points.
-    # corners = cv2.goodFeaturesToTrack(img, 30, 0.2, 5)
-    # for corner in corners:
-    #     x,y = corner.ravel()
-    #     cv2.circle(img,(x,y),5,255,-1)
-
-    #     contours, _ = cv2.findContours(img, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-
-    #     # Find the rotated rectangles and ellipses for each contour
-    #     minRect = [None]*len(contours)
-    #     minEllipse = [None]*len(contours)
-    #     for i, c in enumerate(contours):
-    #         minRect[i] = cv2.minAreaRect(c)
-    #         if c.shape[0] > 5:
-    #             minEllipse[i] = cv2.fitEllipse(c)
-
-    #     # Draw contours + rotated rects + ellipses
-    #     ## [zeroMat]
-    #     drawing = np.zeros((img.shape[0], img.shape[1], 3), dtype=np.uint8)
-    #     ## [zeroMat]
-    #     ## [forContour]
-    #     for i, c in enumerate(contours):
-    #         color = (0,0,255)
-    #         # contour
-    #         cv2.drawContours(drawing, contours, i, color)
-    #         # ellipse
-    #         if c.shape[0] > 5:
-    #             cv2.ellipse(drawing, minEllipse[i], color, 2)
-    #         # rotated rectangle
-    #         box = cv2.boxPoints(minRect[i])
-    #         box = np.intp(box) #np.intp: Integer used for indexing (same as C ssize_t; normally either int32 or int64)
-    #         cv2.drawContours(drawing, [box], 0, color)
-    #     ## [forContour]
-
-    #     ## [showDrawings]
-    #     # Show in a window
-    #     cv2.imshow('Contours', drawing)
-        ## [showDrawings]
-
-    return img
-
-
 if __name__ == '__main__':
     # produce sample image
-    
-    
     img, img_color = image_maker(**args)
     cv2.imshow('Original Image', img)
 
-    # find edges with laplacian filter
-    # img = laplacian_filter(img)
-    # cv2.imshow('Laplacian Image', img)
+    # find corners and other critical points and show them with circles.
+    image = img.copy()
+    img, find_important_points(imagimagee, param=0.4, max_points = 30):
 
-    # apply segmentation
-    circles, output = circle_finder(img, img_color)
+
+    print(corners)
+    # find rectangles.
+    # new_corners = find_squares(img, img_color, corners)
+    # print(len(new_corners))
+    # show critical points without the squares.
+    # draw_corners(img.copy(), new_corners, label='critical points with no square')
 
     # cv2.imshow('Hough Linear', img)
-    create_template(img, img_color)
+    # find_shapes(img)
 
     cv2.waitKey(0)
     cv2.destroyAllWindows()
---
+++ utils.py
@@ -0,0 +1,151 @@
+# utility functions
+
+# import dependencies
+import numpy as np
+import cv2
+
+
+def find_contour_center(c):  
+    ''' finds the center of a contour
+    '''
+    M = cv.moments(c)
+    x = int((M["m10"] / M["m00"]))
+    y = int((M["m01"] / M["m00"]))
+    return (x, y)
+
+
+def pixel_distance(a, b):
+    ''' distance between two points in pixels
+    '''
+    return np.sqrt((a[0] - b[0])**2 + (a[1] - b[1])**2)
+
+
+def inner_product(ab, ac):
+    ''' inner product of two vectors
+    '''
+    return ac[0]*ab[0] + ac[1]*ab[1]
+
+
+def create_vector(a,b):
+    ''' takes two sides and create a vector
+    '''
+    return [b[0]-a[0], b[1]-a[1]]
+
+
+def find_angle(ab, ac):
+    ''' cosine of angle between vectors ab and ac
+    '''
+    ab_length = np.sqrt(inner_product(ab, ab))
+    ac_length = np.sqrt(inner_product(ac, ac))
+    # add a small value to the denominator to avoid division by 0
+    epsilon = 1e-2
+    cos_theta = inner_product(ab, ac) / ((ab_length * ac_length) + epsilon)
+
+    return np.arccos(cos_theta)/np.pi*180
+
+def find_min_max_xy(xy_list):
+    ''' given a list find of (x,y)'s find min of x and y
+    '''
+    min_x = min(xy_list, key=lambda x:x[0])[0]
+    max_x = max(xy_list, key=lambda x:x[0])[0]
+    min_y = min(xy_list, key=lambda x:x[1])[1]
+    max_y = max(xy_list, key=lambda x:x[1])[1]
+    return min_x, min_y, max_x, max_y
+
+
+def sort_corners(pnts):
+    ''' sort corners clock-wise from bottom-left
+    '''
+    a = pnts[0][0]
+    b = pnts[1][0]
+    c = pnts[2][0]
+    d = pnts[3][0]
+    # find min and max of x and y
+    min_x, min_y, max_x, max_y = find_min_max_xy([a,b,c,d])
+
+    # find bottom left point
+    min_distance = np.inf
+    for pnt in [a,b,c,d]:
+        cur_distance = pixel_distance([min_x, min_y] ,pnt)
+        if min_distance > cur_distance:
+            bottom_left = pnt
+            min_distance = cur_distance
+
+    # find top left point
+    min_distance = np.inf
+    for pnt in [a,b,c,d]:
+        cur_distance = pixel_distance([min_x, max_y] ,pnt)
+        if min_distance > cur_distance:
+            top_left = pnt
+            min_distance = cur_distance
+
+    # find top right point
+    min_distance = np.inf
+    for pnt in [a,b,c,d]:
+        cur_distance = pixel_distance([max_x, max_y] ,pnt)
+        if min_distance > cur_distance:
+            top_right = pnt
+            min_distance = cur_distance
+
+    # find bottom right point
+    min_distance = np.inf
+    for pnt in [a,b,c,d]:
+        cur_distance = pixel_distance([max_x, min_y] ,pnt)
+        if min_distance > cur_distance:
+            bottom_right = pnt
+            min_distance = cur_distance
+
+    return [bottom_left, top_left, top_right, bottom_right]
+
+
+def find_sides(sorted_corners):
+    ''' find four sides
+    '''
+    sorted_sides = []
+    sorted_sides.append(create_vector(sorted_corners[0],sorted_corners[1]))
+    sorted_sides.append(create_vector(sorted_corners[1],sorted_corners[2]))
+    sorted_sides.append(create_vector(sorted_corners[2],sorted_corners[3]))
+    sorted_sides.append(create_vector(sorted_corners[3],sorted_corners[0]))
+    return sorted_sides
+
+
+def find_side_lengths(sorted_corners):
+    ''' find length of sides
+    '''
+    dst = []
+    dst.append(pixel_distance(sorted_corners[0], sorted_corners[1]))
+    dst.append(pixel_distance(sorted_corners[1], sorted_corners[2]))
+    dst.append(pixel_distance(sorted_corners[2], sorted_corners[3]))
+    dst.append(pixel_distance(sorted_corners[3], sorted_corners[0]))
+    return dst
+
+
+def find_poly_angles(sorted_sides):
+    ''' find angles
+    '''
+    angles = []
+    angles.append(find_angle(sorted_sides[0], sorted_sides[1]))
+    angles.append(find_angle(sorted_sides[1], sorted_sides[2]))
+    angles.append(find_angle(sorted_sides[2], sorted_sides[3]))
+    angles.append(find_angle(sorted_sides[3], sorted_sides[0]))
+    return angles
+
+
+def check_if_square(approx_cnt, side_tol=0.1, angle_tol=[70,100]):
+    ''' helper function to check approxiated contour is square
+    '''
+    result = False
+    # sort sides and angles
+    sorted_corners = sort_corners(approx_cnt)
+    sorted_sides = find_sides(sorted_corners)
+    side_lengths = find_side_lengths(sorted_corners)
+    angles = find_poly_angles(sorted_sides)
+
+    # check if rectangular
+    if min(angles) > angle_tol[0] and max(angles) < angle_tol[1]:
+        # check if square (sides are close)
+        side_lengths = np.array(side_lengths)
+        tol = side_lengths.mean()*side_tol
+        result = np.allclose(side_lengths, side_lengths.mean(), atol = tol)
+    return result
+
